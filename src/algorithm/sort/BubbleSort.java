0043aab246211a6aaadf45b08b0cb42dff54d654
jjorae <raehyeok.cho@gmail.com>
2021-05-12 23:47:11 +0900

    버블정렬 예제 추가

---
+++ src/algorithm/sort/BubbleSort.java
@@ -0,0 +1,17 @@
+package algorithm.sort;
+
+public class BubbleSort {
+    public int[] sort(int[] targetArray) {
+        for(int i = 0; i < targetArray.length; i++) {
+            for(int j = 0; j < targetArray.length - 1; j++) {
+                if(targetArray[j] > targetArray[j + 1]) {
+                    int tmp = targetArray[j];
+                    targetArray[j] = targetArray[j + 1];
+                    targetArray[j + 1] = tmp;
+                }
+            }
+        }
+
+        return targetArray;
+    }
+}
