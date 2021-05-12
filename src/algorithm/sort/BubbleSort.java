882c39598c22bcd7b6689b9880339154b4af7101
jjorae <raehyeok.cho@gmail.com>
2021-05-13 00:26:19 +0900

    버블정렬 시 n-1회 loop로 변경

--- src/algorithm/sort/BubbleSort.java
+++ src/algorithm/sort/BubbleSort.java
@@ -2,7 +2,7 @@ package algorithm.sort;
 
 public class BubbleSort {
     public int[] sort(int[] targetArray) {
-        for(int i = 0; i < targetArray.length; i++) {
+        for(int i = 0; i < targetArray.length - 1; i++) {
             for(int j = 0; j < targetArray.length - 1; j++) {
                 if(targetArray[j] > targetArray[j + 1]) {
                     int tmp = targetArray[j];
